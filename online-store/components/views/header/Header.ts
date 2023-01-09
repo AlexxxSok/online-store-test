a4983ee2957c5c197aed2754bc0b100583f16dc8
Pavel-Troinich <tr.pav.ar@gmail.com>
2023-01-09 22:33:33 +0300

    fix: fix hash

--- online-store/components/views/header/Header.ts
+++ online-store/components/views/header/Header.ts
@@ -4,7 +4,7 @@ class Header {
     const header = <HTMLElement>document.querySelector('header');
     const html = `<div class="header__content">
                 <div class="header__top">
-                  <h1 class="main-title"><a href="/#">technical store</a></h1>
+                  <h1 class="main-title"><a href="#">technical store</a></h1>
                   <div class="total">
                     <p class="total-title">Total</p>
                     <div class ="total-sum">${sum}</div>
